3d5030427913848e400bcfa88f378d6645695898
TadeQ <88510018+TadeQQ@users.noreply.github.com>
2023-04-06 18:36:22 +0200

    feat: add override GetLetterGrade

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,6 +1,7 @@
 ﻿using GradeBook.Enums;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace GradeBook.GradeBooks
@@ -10,6 +11,38 @@ namespace GradeBook.GradeBooks
         public RankedGradeBook(string name) : base(name)
         {
             Type = GradeBookType.Ranked;
+        }
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException();
+            }
+
+            int topTwentyPercent = (int)Math.Ceiling(Students.Count * 0.2);
+
+            List<double> grades = Students.OrderByDescending(s => s.AverageGrade).Select(s => s.AverageGrade).ToList();
+
+            if (averageGrade >= grades[topTwentyPercent - 1])
+            {
+                return 'A';
+            }
+            else if (averageGrade >= grades[(topTwentyPercent * 2) - 1])
+            {
+                return 'B';
+            }
+            else if (averageGrade >= grades[(topTwentyPercent * 3) - 1])
+            {
+                return 'C';
+            }
+            else if (averageGrade >= grades[(topTwentyPercent * 4) - 1])
+            {
+                return 'D';
+            }
+            else
+            {
+                return 'F';
+            }
         }
     }
 }
